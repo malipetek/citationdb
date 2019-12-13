1cff4762f6a98b747766072232176772e6b6759e
Jake Kara <jake@jakekara.com>
2019-12-13 10:25:56 -0500

    added config file, plus

---
+++ src/CitationDB/config.js
@@ -0,0 +1,12 @@
+export default {
+    histogram:{
+        minYear: 1975,
+        maxYear: 2020
+    },
+    words:{
+        resource:{
+            singular: "testimony",
+            plural: "testimonies"
+        }
+    }
+}
